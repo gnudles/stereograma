1984b5c614d05b7e19076b095e933c3f9a704117
Orr Shalon Dvory <dvoreader@gmail.com>
2019-07-11 17:51:03 +0300

    moved to ExprTk in Depth map generation

--- FormulaGen.cpp
+++ FormulaGen.cpp
@@ -1,8 +1,12 @@
 #include "FormulaGen.h"
 #include "ui_FormulaGen.h"
-#include "parse.h"
+#include "exprtk/exprtk.hpp"
 #include "stereomaker.h"
 #include "mainwindow.h"
+
+typedef exprtk::symbol_table<float> symbol_table_t;
+typedef exprtk::expression<float> expression_t;
+typedef exprtk::parser<float> parser_t;
 
 FormulaGen::FormulaGen(QWidget *parent) :
 	QMainWindow(parent),
@@ -36,14 +40,33 @@ void FormulaGen::on_generatePb_clicked()
 	const char* formula_buf=formula_ba.constData();
 	imdata=QImage(w,h,QImage::Format_RGB32);
     
+    float x_s, y_s;
+    symbol_table_t symbol_table;
+    symbol_table.add_pi();
+    symbol_table.add_constant("e", M_E);
+    symbol_table.add_variable("x", x_s);
+    symbol_table.add_variable("y", y_s);
+    expression_t expression;
+
+    expression.register_symbol_table(symbol_table);
+            parser_t::settings_store sstore;
+            sstore.disable_all_logic_ops();
+            sstore.disable_all_assignment_ops();
+            sstore.disable_all_control_structures();
+            parser_t parser(sstore);
+
+    bool compile_valid=parser.compile(formula_buf, expression);
 	bool ok;
 	int val;
     for (int x=0;x<w;++x)
 	{
 		for (int y=0;y<h;++y)
 		{
-			val=parseXY(formula_buf,(float)x/w,(float)y/h,&ok)*255.99;
-			if (val<0)
+            x_s = (float)x/w;
+            y_s = (float)y/h;
+            val=expression.value()*255.99;
+
+            if (std::isnan(val) || std::isinf(val) || val<0)
 				val=0;
 			else if (val>255)
 				val=255;
